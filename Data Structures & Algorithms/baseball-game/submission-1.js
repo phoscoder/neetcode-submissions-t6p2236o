f74428fba8a8aceb2cfffa1cb39407ca39920654
Victor Phos <victorphos1@gmail.com>
2026-04-09 11:09:16 +0200

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations) {
+        let ops = []
+
+        for (const op of operations){
+
+            console.log("Ops: ", ops)
+            if (op == 'D')ops.push(parseFloat(ops[ops.length - 1] * 2))
+            else if (op == '+') ops.push(parseFloat(ops[ops.length - 1]) + parseFloat(ops[ops.length - 2]))
+            else if (op == 'C') ops.pop()
+            else ops.push(parseFloat(op))
+            
+
+        }
+
+        return ops.reduce((ps, a) => parseInt(ps) + a, 0)
+    }
+
+
+}
