d81c449741596e4d3117cadfef0b1ecb0c99c8b7
Victor Phos <victorphos1@gmail.com>
2026-04-09 12:25:32 +0200

    Add: minimum-stack - submission-25

---
+++ Data Structures & Algorithms/minimum-stack/submission-25.js
@@ -0,0 +1,25 @@
+class MinStack {
+    stack 
+    min
+    constructor(
+
+    ) {
+        this.stack = []
+    }
+
+    push(val) {
+        this.stack.push(val)
+    }
+
+    pop() {
+        return  this.stack.pop()
+    }
+
+    top() {
+        return this.stack[this.stack.length -1]
+    }
+
+    getMin() {
+        let min = this.stack[0]; this.stack.forEach(i => { i < min ? min = i: null}); return min
+    }
+}
